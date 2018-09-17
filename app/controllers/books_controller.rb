40b1207b7fab4cd0404c79cc05b2ed654ebe356f
山岸宗奨 <yamagishi@yamagishisoususumunoMacBook-puro.local>
2018-09-17 17:17:42 +0900

    修正3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -24,7 +24,7 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to books_path
+    redirect_to books_path, notice: "successfully"
   end
 
   def top
@@ -33,7 +33,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to books_path
+    redirect_to books_path, notice: "successfully"
   end
 
   private
